e8734d046e5d74bebb90b452841ff2247fe6925e
ScriptedRain <71009876+ScriptedRain@users.noreply.github.com>
2021-04-16 18:17:01 +1000

    Tasks component :+1:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import './App.css';
 import Header from './components/Header';
+import Tasks from './components/Tasks'
+import { useState } from 'react'
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+    id: 1,
+    text: 'mom Appointment',
+    day: 'today',
+    reminder: true,
+    },
+    {
+      id: 2,
+      text: 'dad Appointment',
+      day: 'todady',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'school Appointment',
+      day: 'friday',
+      reminder: false,
+    },
+  ])
+
   return (
     <div className="container">
       <Header title={1} />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
