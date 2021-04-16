35f81085b077dba1659b735a75415af7744e107f
ScriptedRain <71009876+ScriptedRain@users.noreply.github.com>
2021-04-16 21:02:13 +1000

    Finish

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 
 
-const Button = ({ color, text, onClick  }) => {
+const Button = ({ color, text, onClick }) => {
   return (
     <button style={{ backgroundColor: color}} className='btn' onClick={onClick}>{text}</button>
   )
